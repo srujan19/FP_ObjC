22143f8438201addec42dc07121a09c2b0cb6f60
Srujan Simha Adicharla <srujansimhaadicharla@srujans-macbook-pro.local>
2015-12-16 17:13:29 -0600

    Initial Commit

---
+++ FP_ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FP_ObjC
+//
+//  Created by Srujan Simha Adicharla on 12/16/15.
+//  Copyright © 2015 Srujan Simha Adicharla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FP_ObjCTests/FP_ObjCTests.m
@@ -0,0 +1,39 @@
+//
+//  FP_ObjCTests.m
+//  FP_ObjCTests
+//
+//  Created by Srujan Simha Adicharla on 12/16/15.
+//  Copyright © 2015 Srujan Simha Adicharla. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FP_ObjCTests : XCTestCase
+
+@end
+
+@implementation FP_ObjCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
